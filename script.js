f5bea91cd0946ad737bb304a139afa8574df8701
Jamey Gronewald <jrgronewald@gmail.com>
2020-06-13 13:10:02 -0700

    copied over HTML and CSS from template, added current date and current time to header section of HTML

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('HELLO WORLD');
+let dateHeader = document.querySelector('#currentDay');
+let timeHeader = document.querySelector('#currentTime');
+
+dateHeader.textContent = moment().format('dddd MMMM do, YYYY');
+timeHeader.textContent = moment().format('LTS');
+console.log(dateHeader);
