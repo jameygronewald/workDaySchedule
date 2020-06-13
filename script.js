36a6ce2ea8e5d9cceb40b97d3d923b9b83b01a5e
Jamey Gronewald <jrgronewald@gmail.com>
2020-06-13 14:14:35 -0700

    initialized array variable for hours from 9-5; wrote function displayTimeBlocks to generate rows for each block of time representing hours from 9am to 5pm with button to save data

--- script.js
+++ script.js
@@ -1,6 +1,52 @@
-let dateHeader = document.querySelector('#currentDay');
-let timeHeader = document.querySelector('#currentTime');
+// Current date and time for header
+$('#currentDay').text(moment().format('dddd MMMM do, YYYY'));
+$('#currentTime').text(moment().format('LTS'));
 
-dateHeader.textContent = moment().format('dddd MMMM do, YYYY');
-timeHeader.textContent = moment().format('LTS');
-console.log(dateHeader);
+{/*     <div class="timeBlock">
+          <div class="row">
+            <div class="col-2 hour">
+                
+            </div>
+            <div class="col-8 past">
+
+            </div>
+            <div class="col-2 saveBtn">
+
+            </div>
+          </div>
+        </div>      */}
+
+const workDayHours = [
+    '9:00AM',
+    '10:00AM',
+    '11:00AM',
+    '12:00PM',
+    '1:00PM',
+    '2:00PM',
+    '3:00PM',
+    '4:00PM',
+    '5:00PM'
+]
+// Functions
+const displayTimeBlocks = function() {
+    let i = 0;
+    while (i < workDayHours.length) {
+        let timeBlock = $('<div>').addClass('timeBlock');
+        let row = $('<div>').addClass('row');
+        let hourColumn = $('<div>').addClass('col-1 hour');
+        let activitiesColumn = $('<div>').addClass('col-10');
+        let saveColumn = $('<div>').addClass('col-1 saveBtn');
+        $(row).append(hourColumn.text(workDayHours[i]));
+        $(row).append(activitiesColumn);
+        $(row).append(saveColumn);
+        $(timeBlock).append(row);
+        $('.container').append(timeBlock);
+        i++;
+    }
+};
+
+
+// Function Calls
+displayTimeBlocks();
+
+// Event Listeners
